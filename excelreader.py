765580eb2e89b3a0e4c03dfb130c48016f9a6fe4
Bonse <84657555+OsuBonse@users.noreply.github.com>
2023-02-13 21:32:53 +0300

    Update excelreader.py

--- excelreader.py
+++ excelreader.py
@@ -1,6 +1,6 @@
 from openpyxl import *
-wb = load_workbook(r'F:\Prog\Py\b1.xlsx')
-sheet_ranges = wb['123']
+wb = load_workbook(r'...')
+sheet_ranges = wb['...']
 for row in sheet_ranges.iter_rows(min_col=1, max_col=6):
     for cell in row:
         print(cell.value)
